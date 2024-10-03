e0d4c4d3cef168029c2ee62b42c90930fb759b76
chubebanso <vuquanghoanglam@gmail.com>
2024-10-03 23:26:11 +0700

    response entity

---
+++ src/main/java/vn/chubebanso/icecream/domain/RestResponse.java
@@ -0,0 +1,41 @@
+package vn.chubebanso.icecream.domain;
+
+public class RestResponse<T> {
+    private int statusCode;
+    private String errCode;
+    private Object message;
+    private T data;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+}
---
+++ src/main/java/vn/chubebanso/icecream/util/IdInvalidException.java
@@ -0,0 +1,7 @@
+package vn.chubebanso.icecream.util;
+
+public class IdInvalidException extends Exception {
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
