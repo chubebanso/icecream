60a71c831f0ce429e9fd83a5f1e5c2f20e2c2a98
hinmfm <minhsatwork@gmail.com>
2024-10-28 09:09:33 +0700

    Update ProductService.java

--- src/main/java/vn/chubebanso/icecream/service/ProductService.java
+++ src/main/java/vn/chubebanso/icecream/service/ProductService.java
@@ -17,8 +17,7 @@ public class ProductService {
     private final CartItemRepository cartItemRepository;
     private final CartService cartService;
 
-    public ProductService(ProductRepository productRepository, CartItemRepository cartItemRepository,
-            CartService cartService) {
+    public ProductService(ProductRepository productRepository, CartItemRepository cartItemRepository, CartService cartService) {
         this.productRepository = productRepository;
         this.cartItemRepository = cartItemRepository;
         this.cartService = cartService;
