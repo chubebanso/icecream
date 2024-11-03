7e4156758bc53d9dcc2515d0fb2a5b7bbe27a98a
chubebanso <vuquanghoanglam@gmail.com>
2024-11-03 20:48:50 +0700

    user-pagination

--- src/main/java/vn/chubebanso/icecream/repository/UserRepository.java
+++ src/main/java/vn/chubebanso/icecream/repository/UserRepository.java
@@ -3,6 +3,7 @@ package vn.chubebanso.icecream.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import vn.chubebanso.icecream.domain.ResultPaginationDTO;
 import vn.chubebanso.icecream.domain.User;
 
 @Repository
